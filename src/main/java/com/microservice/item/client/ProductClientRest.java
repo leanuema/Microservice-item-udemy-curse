7abccb0474113b3b3c919b03f574b723d29326d0
leanuema <leanuema@hotmail.com>
2021-03-03 21:41:33 -0300

    fix(pom): adding feign dependency

--- src/main/java/com/microservice/item/client/ProductClientRest.java
+++ src/main/java/com/microservice/item/client/ProductClientRest.java
@@ -2,11 +2,13 @@ package com.microservice.item.client;
 
 import com.microservice.item.models.Item;
 import com.microservice.item.models.Product;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+@FeignClient(name = "product-service", url = "localhost:8001")
 public interface ProductClientRest {
 
     @GetMapping(value = "/listar")
